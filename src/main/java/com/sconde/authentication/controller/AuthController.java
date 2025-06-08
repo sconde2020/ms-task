0d933c1d1971352704297e6dc51783b080edd477
sconde2020 <souleymaneconde2020@gmail.com>
2025-06-08 14:40:50 +0200

    Refactoring authentication api

--- src/main/java/com/sconde/authentication/controller/AuthController.java
+++ src/main/java/com/sconde/authentication/controller/AuthController.java
@@ -1,84 +1,24 @@
 package com.sconde.authentication.controller;
 
-import com.sconde.authentication.model.Role;
 import com.sconde.authentication.model.User;
-import com.sconde.authentication.repository.UserRepository;
-import com.sconde.authentication.service.JwtService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-@RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Authentication API", description = "Operations related to authentication")
-public class AuthController {
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
-    private final Set<String> invalidatedTokens = new HashSet<>();
-
-    public AuthController(AuthenticationManager authenticationManager,
-                          UserRepository userRepository,
-                          PasswordEncoder passwordEncoder,
-                          JwtService jwtService) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
-    }
+public interface AuthController {
 
     @PostMapping("/register")
     @Operation(summary = "Register user")
-    public ResponseEntity<?>  register(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole(Role.USER);
-        userRepository.save(user);
-        return ResponseEntity.ok(Map.of("message", "User registered"));
-    }
+    public ResponseEntity<?>  register(@RequestBody User user);
 
     @PostMapping("/login")
     @Operation(summary = "Login user")
-    public ResponseEntity<?> login(@RequestBody User user) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            // Extract authenticated user details
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            String token = jwtService.generateToken(userDetails.getUsername());
-
-            // Return token in JSON format
-            return ResponseEntity.ok(Collections.singletonMap("token", token));
-
-        } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.singletonMap("error", "Invalid username or password"));
-        }
-    }
+    public ResponseEntity<?> login(@RequestBody User user);
 
     @PostMapping("/logout")
     @Operation(summary = "Logout user")
-    public ResponseEntity<?> logout(@RequestHeader("Authorization") String token) {
-        if (token.startsWith("Bearer ")) {
-            token = token.substring(7);
-        }
-        invalidatedTokens.add(token);
-        return ResponseEntity.ok(Collections.singletonMap("message", "Logged out successfully"));
-    }
+    public ResponseEntity<?> logout(@RequestHeader("Authorization") String token);
 }
